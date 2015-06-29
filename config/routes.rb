d0e6f40ce44235648365b4ebf3009ba582024497
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-29 16:22:35 -0500

    ruby set-up redo

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
+  resources :users
+ 
+
+root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+get 'login' => 'user_sessions#new', :as => :login
+post 'logout' => 'user_sessions#destroy', :as => :logout
+end

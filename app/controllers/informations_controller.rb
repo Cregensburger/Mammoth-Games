c64aa5538847caf6f8ff3107cf70296c08a8c69f
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-29 21:10:38 -0500

    login and page connections abound

---
+++ app/controllers/informations_controller.rb
@@ -0,0 +1,5 @@
+class InformationsController < ApplicationController
+	skip_before_filter :require_login, only: [:index]
+	def index
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
+  get 'games/index'
+
   get 'user_sessions/new'
 
   get 'user_sessions/create'
 
   get 'user_sessions/destroy'
 
+  get 'informations/index'
+
   resources :users
  
 
-root :to => 'users#index'
+root :to => 'games#index'
+  resources :games
   resources :user_sessions
   resources :users
+
 
 get 'login' => 'user_sessions#new', :as => :login
 post 'logout' => 'user_sessions#destroy', :as => :logout
